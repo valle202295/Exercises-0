47e037d1e938648ae475707678c7b6b77510ae39
deltagare <deltagare@DESKTOP-GCE7HLI>
2022-02-07 19:14:39 +0100

    Add project files.

---
+++ Exercises 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercises_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("You successfully ran exercise one!");
+            Console.WriteLine("You successfully ran exercise two!");
+
+            Console.Write("Enter assignment number or (- 1 to exit): ");
+            var assignmentChoice = int.Parse(Console.ReadLine() ?? "");
+            Console.ForegroundColor = ConsoleColor.Green;
+            switch (assignmentChoice)
+         
+
+            
+
+
+
+
+            
+        }
+    }
+}
